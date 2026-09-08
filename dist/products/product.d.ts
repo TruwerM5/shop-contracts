b1cefcefd93bf75c2a38e43b1f943ec10455fc19
halimov <halimov@infomatika.ru>
2026-09-08 17:29:22 +0300

    add product types

---
+++ dist/products/product.d.ts
@@ -0,0 +1,24 @@
+export interface ProductResponse {
+    productId: number;
+    name: string;
+    price: number;
+    rating: number;
+    category: ProductCategory;
+    sellerId: number;
+    productImages: ProductImagesResponse[];
+}
+export interface ProductImagesResponse {
+    productId: number;
+    imagePath: string;
+}
+export type ProductCategory = "gadgets" | "books" | "clothes";
+export interface FullProductItem extends ProductResponse {
+    productDetails: ProductDetailsResponse;
+}
+export interface ProductDetailsResponse {
+    productId: number;
+    color: string | null;
+    size: string | null;
+    author: string | null;
+    description?: string;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './cart/cart';
 export * from './auth/user';
+export * from './products/product';
